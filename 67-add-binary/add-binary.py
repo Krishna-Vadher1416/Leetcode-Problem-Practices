472db75ba6f0e108ac7521c7311b37030abf2cc8
Krishna-Vadher1416 <krishna.vadher1416@gmail.com>
2026-07-08 20:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (16.60%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,3 +1,10 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,2)+ int(b,2))[2:]
+        x, y = int(a, 2), int(b, 2)
+        
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+            
+        return bin(x)[2:]
