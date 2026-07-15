de2db98895a24ed656f2eb2588ca58049f572aaf
Krishna-Vadher1416 <krishna.vadher1416@gmail.com>
2026-07-15 18:22:48 +0530

    Time: 7 ms (53.71%) | Memory: 26.6 MB (43.98%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -8,11 +8,18 @@ class Solution:
          
         k = k % len(nums)
 
-        nums.reverse()
+        def reverse(start : int , end : int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start+= 1
+                end -= 1
 
-        nums[:k] = reversed(nums[:k])
 
-        nums[k:] = reversed(nums[k:])
+        reverse(0,len(nums)-1 )
+        reverse(0, k-1)
+        reverse(k , len(nums)- 1)
+
+        
 
 
 
