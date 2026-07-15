072ee575729cdaea678296466e7da0be5479b9ab
Krishna-Vadher1416 <krishna.vadher1416@gmail.com>
2026-07-15 17:58:24 +0530

    Time: 4 ms (24.01%) | Memory: 20.5 MB (42.21%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,9 +4,12 @@ class Solution:
             return 0
 
         i = 0
-        for j in range(1,len(nums)):
+        j = 0
+        while j < len(nums):
             if nums[j] != nums[i]:
                 i += 1
                 nums[i] = nums[j]
+
+            j += 1
 
         return i+1
