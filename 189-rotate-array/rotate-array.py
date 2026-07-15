7d632579aac45655d056d128a6d3ffa8137676a7
Krishna-Vadher1416 <krishna.vadher1416@gmail.com>
2026-07-15 18:17:14 +0530

    Time: 3 ms (73.52%) | Memory: 26.5 MB (43.98%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k == 0:
+            return 0    
+        n = len(nums) 
+        k = k % n
+
+        nums.reverse()
+
+        nums[:k] = reversed(nums[:k])
+
+        nums[k:] = reversed(nums[k:])
+
+
+
+
+
+
